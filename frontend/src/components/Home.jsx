48039dc29612a7acc20be5c4666c5ef60a9aa2c1
JR-Koders <jules.caoeiros@gmail.com>
2024-06-04 20:39:16 -0400

    Home page logic + small bug fix
    Home page that displays "Hello ${username}"
    Fixed bug of react-cookie's Cookie.remove('token') not working and causing malfunction in login system (like logging out after a few seconds if logging in after clicking log out button)

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,7 +1,56 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
 
 const Home = () => {
-  return <h1>Home PAGE</h1>;
+    const navigate = useNavigate();
+    const [username, setUsername] = useState("");
+    useEffect(() => {
+        const verifyCookie = async () => {
+            if (!Cookies.get('token')) {
+                navigate("/login");
+            }
+            const { data } = await axios.post(
+                "http://localhost:5555/",
+                {},
+                { withCredentials: true }
+            );
+            const { status, user } = data;
+            setUsername(user);
+            return status
+                ? toast(`Hello ${user}`, {
+                    position: "top-right",
+                })
+                : (
+                    Cookies.remove('token'),
+                    navigate("/login"));
+        };
+        verifyCookie();
+    }, []);
+    const Logout = () => {
+        Cookies.remove('token');
+        navigate("/login");
+    };
+    return (
+        <>
+            <div className="flex items-center justify-center min-h-screen bg-gray-100">
+                <div className="bg-white p-8 rounded-lg shadow-lg">
+                    <h4 className="text-xl font-bold mb-4">
+                        Welcome <span className="text-indigo-600">{username}</span>
+                    </h4>
+                    <button
+                        onClick={Logout}
+                        className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                    >
+                        LOGOUT
+                    </button>
+                </div>
+            </div>
+            <ToastContainer />
+        </>
+    );
 };
 
-export default Home
+export default Home;
